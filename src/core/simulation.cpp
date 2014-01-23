6bf8e1a82d38e604c3b87743ef5dcf4f8bf0ac69
willdavis <bdavis84@pacbell.net>
2014-01-22 23:08:24 -0800

    added min and max variables to try and regulate the simulations framerate

--- src/core/simulation.cpp
+++ src/core/simulation.cpp
@@ -13,15 +13,13 @@ namespace core
 
   Simulation::Simulation()
   {
-    event_manager.set_parent((void*)this);
-    state = 0;          //set default state
-    time_scale = 1;     //set time scale to normal time (1x)
-
-    real_elapsed_time = 0;      // set elapsed real time to zero
+    event_manager.set_parent((void*) (this));
+    state = 0; //set default state
+    time_scale = 1; //set time scale to normal time (1x)
+    real_elapsed_time = 0; // set elapsed real time to zero
     simulated_elapsed_time = 0; // set elapsed simulation time to zero
-
-    real_duration = 0;          //default to infinite duration (wait for exit event)
-    simulated_duration = 0;     //default to infinite duration (wait for exit event)
+    real_duration = 0; //default to infinite duration (wait for exit event)
+    simulated_duration = 0; //default to infinite duration (wait for exit event)
   }
 
   Simulation::~Simulation()
@@ -29,38 +27,40 @@ namespace core
     // TODO Auto-generated destructor stub
   }
 
-  void Simulation::exit()
+  void
+  Simulation::exit()
   {
     state = 1;
   }
 
-  void Simulation::run()
+  void
+  Simulation::run()
   {
-    while(state == 0) {
-      // first check if we're over the duration.
-      // if so, schedule an ExitEvent for immediate termination of the simulation!
-      if(real_elapsed_time >= real_duration and real_duration > 0) {
-        event_manager.schedule_event(new ExitEvent);
-      }
-
-      // whew! we're not done yet!
-      // update time between last cycle and this one.
-      else {
-        uint64_t dt = core_timer.get_elapsed_time();      // get delta time between last frame (nanoseconds)
-        real_elapsed_time += dt;
-      }
-
-      // process the event queue for this cycle
-      int queue_size = event_manager.get_queue_size();
-      while(queue_size > 0)
+    while (state == 0)
       {
-        event_manager.process_top_event();
-        queue_size = event_manager.get_queue_size();
+        // first check if we're over the duration.
+        // if so, schedule an ExitEvent for immediate termination of the simulation!
+        if (real_elapsed_time >= real_duration && real_duration > 0)
+          {
+            event_manager.schedule_event(new ExitEvent);
+          }
+        else // whew! we're not done yet!
+        // update time between last cycle and this one.
+          {
+            uint64_t dt = core_timer.get_elapsed_time(); // get delta time between last frame (nanoseconds)
+            real_elapsed_time += dt;
+          }
+        // process the event queue for this cycle
+        int queue_size = event_manager.get_queue_size();
+        while (queue_size > 0)
+          {
+            event_manager.process_top_event();
+            queue_size = event_manager.get_queue_size();
+          }
+        // is the event queue empty?
+        // if so, set the simulation to the idle state
       }
 
-      // is the event queue empty?
-      // if so, set the simulation to the idle state
-    }
   }
 
   // schedule event with EventManager and return the new queue size
@@ -69,6 +69,18 @@ namespace core
     event_manager.schedule_event(event);
     return event_manager.get_queue_size();
   }
+
+  // returns the minimum time desired for each frame
+  uint64_t Simulation::get_min_time_per_frame() { return min_time_per_frame; }
+
+  // returns the maximum time desired for each frame
+  uint64_t Simulation::get_max_time_per_frame() { return max_time_per_frame; }
+
+  // sets the minimum time desired for each frame
+  void Simulation::set_min_time_per_frame(uint64_t new_time) { min_time_per_frame = new_time; }
+
+  // set the maximum time desired for each frame
+  void Simulation::set_max_time_per_frame(uint64_t new_time) { max_time_per_frame = new_time; }
 
   // returns the simulated duration (arbitrary units)
   uint64_t Simulation::get_simulated_duration() { return simulated_duration; }
