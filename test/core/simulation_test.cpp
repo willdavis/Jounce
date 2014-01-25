26da4a88b98300186490d572fae526c64ae22f1d
willdavis <bdavis84@pacbell.net>
2014-01-25 00:48:16 -0800

    improved integration tests

--- test/core/simulation_test.cpp
+++ test/core/simulation_test.cpp
@@ -97,41 +97,11 @@ namespace core
   }
 
   TEST_F(SimulationTest, simulation_runs_for_duration_then_exits) {
-    sim.set_real_and_sim_duration(1000000, 50);
-    timespec start, end;        //to track the actual time offset
-    /*
-    timespec t1,t2,t3,t4,t5;    //used to calibrate average time for clock_gettime() call
-    int scale = 1;
-    int count = scale;
-    int sum_of_gettime = 0;
-    while(count > 0) {
-      //calibrate average time for clock_gettime
-      clock_gettime(1, &t1);
-      clock_gettime(1, &t2);
-      clock_gettime(1, &t3);
-      clock_gettime(1, &t4);
-      clock_gettime(1, &t5);
-
-      timespec* elapsed1 = sim.core_timer.get_timespec_diff(&t2, &t1);
-      timespec* elapsed2 = sim.core_timer.get_timespec_diff(&t3, &t2);
-      timespec* elapsed3 = sim.core_timer.get_timespec_diff(&t4, &t3);
-      timespec* elapsed4 = sim.core_timer.get_timespec_diff(&t5, &t4);
-      sum_of_gettime += elapsed1->tv_nsec + elapsed2->tv_nsec + elapsed3->tv_nsec + elapsed4->tv_nsec;
-
-      count--;
-    }
-    uint64_t average_time_for_gettime = sum_of_gettime / (scale*4);
-    EXPECT_GT(average_time_for_gettime, 0);
-    EXPECT_LT(average_time_for_gettime, 1);
-    */
-    clock_gettime(1, &start);
+    sim.set_real_duration_and_frequency(1000000, 100);
     sim.run();
-    clock_gettime(1, &end);
-
-    timespec* elapsed = sim.core_timer.get_timespec_diff(&end, &start);
-    uint64_t nseconds = elapsed->tv_sec * 1000000000LL + elapsed->tv_nsec;// + average_time_for_gettime;
-    EXPECT_GT(nseconds - sim.get_elapsed_real_time(), 0);
+    EXPECT_EQ(sim.get_sim_duration(), 10000);
     ASSERT_EQ(sim.get_elapsed_real_time(), sim.get_real_duration());
+    ASSERT_EQ(sim.get_elapsed_sim_time(), sim.get_sim_duration());
   }
 
 } /* namespace core */
