48a78c88ac29a2726ebde9ffa5ff61a47107b4a3
willdavis <bdavis84@pacbell.net>
2014-02-02 20:01:30 -0800

    generated updateable interface

---
+++ src/core/interfaces/updateable.cpp
@@ -0,0 +1,24 @@
+/*
+ * updateable.cpp
+ *
+ *  Created on: Feb 2, 2014
+ *      Author: bill
+ */
+
+#include "updateable.h"
+
+namespace core
+{
+
+	Updateable::Updateable()
+	{
+		// TODO Auto-generated constructor stub
+
+	}
+
+	Updateable::~Updateable()
+	{
+		// TODO Auto-generated destructor stub
+	}
+
+} /* namespace core */
