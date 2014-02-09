f97726b6a7ee3146fc1a88784288a6f2cb849327
willdavis <bdavis84@pacbell.net>
2014-02-09 00:07:47 -0800

    implemented frame counter

--- src/diagnostics/frame_counter.cpp
+++ src/diagnostics/frame_counter.cpp
@@ -12,8 +12,17 @@ namespace Diagnostics
 
 	FrameCounter::FrameCounter()
 	{
-		// TODO Auto-generated constructor stub
+		_total_time = 0;
+		_total_frames = 0;
 
+		_shortest_frame = 1000000000;
+		_longest_frame = 0;
+
+		_short_frame = 0;
+		_short_frame_count = 0;
+
+		_long_frame = 0;
+		_long_frame_count = 0;
 	}
 
 	FrameCounter::~FrameCounter()
@@ -21,4 +30,77 @@ namespace Diagnostics
 		// TODO Auto-generated destructor stub
 	}
 
-} /* namespace Diagnostics */
+	void FrameCounter::notify(Observable* signal, std::shared_ptr<Observer> slot)
+	{
+	}
+
+	void FrameCounter::update(uint64_t* dt)
+	{
+		_total_frames++;
+		_total_time += *dt;
+		if(*dt < _shortest_frame){ _shortest_frame = *dt; }
+		if(*dt > _longest_frame){ _longest_frame = *dt; }
+
+		if(*dt < _short_frame){ _short_frame_count++; }
+		if(*dt > _long_frame){ _long_frame_count++; }
+	}
+
+	double FrameCounter::average_frame()
+	{
+		if(_total_frames == 0){ return 0; }
+		return _total_time / _total_frames;
+	}
+
+	double FrameCounter::long_frame_ratio()
+	{
+		if(_total_frames == 0){ return 0; }
+		return _long_frame_count / _total_frames;
+	}
+
+	double FrameCounter::short_frame_ratio()
+	{
+		if(_total_frames == 0){ return 0; }
+		return _short_frame_count / _total_frames;
+	}
+
+	uint64_t FrameCounter::shortest_frame()
+	{
+		return _shortest_frame;
+	}
+
+	uint64_t FrameCounter::longest_frame()
+	{
+		return _longest_frame;
+	}
+
+	uint64_t FrameCounter::total_frames()
+	{
+		return _total_frames;
+	}
+
+	uint64_t FrameCounter::total_time()
+	{
+		return _total_time;
+	}
+
+	uint64_t FrameCounter::short_frame()
+	{
+		return _short_frame;
+	}
+
+	void FrameCounter::set_short_frame(uint64_t* nanoseconds)
+	{
+		_short_frame = *nanoseconds;
+	}
+
+	uint64_t FrameCounter::long_frame()
+	{
+		return _long_frame;
+	}
+
+	void FrameCounter::set_long_frame(uint64_t* nanoseconds)
+	{
+		_long_frame = *nanoseconds;
+	}
+}
+ /* namespace Diagnostics */
--- src/diagnostics/frame_counter.h
+++ src/diagnostics/frame_counter.h
@@ -9,7 +9,7 @@
 #define FRAME_COUNTER_H_
 
 #include <stdint.h>
-#include "./core/interfaces/observer.h"
+#include "../core/objects/object.h"
 
 using namespace core;
 
@@ -25,15 +25,34 @@ namespace Diagnostics
 		void notify(Observable* signal, std::shared_ptr<Observer> slot);
 		void update(uint64_t* dt);
 
-		uint64_t get_average();
+		double average_frame();
+		double long_frame_ratio();
+		double short_frame_ratio();
 
-		uint64_t min_framerate;
-		uint64_t max_framerate;
+		uint64_t shortest_frame();
+		uint64_t longest_frame();
 
-		uint64_t long_frame;
-		uint64_t long_frame_count;
+		uint64_t total_frames();
+		uint64_t total_time();
 
-		uint64_t total_frames;
+		uint64_t short_frame();
+		void set_short_frame(uint64_t* nanoseconds);
+
+		uint64_t long_frame();
+		void set_long_frame(uint64_t* nanoseconds);
+
+	protected:
+		uint64_t _shortest_frame;
+		uint64_t _longest_frame;
+
+		uint64_t _short_frame;
+		uint64_t _short_frame_count;
+
+		uint64_t _long_frame;
+		uint64_t _long_frame_count;
+
+		uint64_t _total_frames;
+		uint64_t _total_time;
 	};
 
 } /* namespace Diagnostics */
