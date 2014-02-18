6ac666f10e28a83c6289d5bb32e32f5f4725bd7f
willdavis <bdavis84@pacbell.net>
2014-02-17 22:41:20 -0800

    JSignal now inherits from JMetaObject

--- src/core/objects/signal.h
+++ src/core/objects/signal.h
@@ -9,29 +9,18 @@
 #define JSIGNAL_H_
 
 #include <map>
-#include "object.h"
+#include "meta_object.h"
 #include "slot.h"
 
 namespace core
 {
 
 	template<typename SignalType = void*>
-	class JSignal
+	class JSignal : public JMetaObject
 	{
 	public:
-		JSignal(JObject* parent, std::string signature)
-		{
-			_parent = parent;
-			_signature = signature;
-		}
-
-		JObject* parent() { return _parent; }
-		std::string signature(){ return _signature; }
+		JSignal(JObject* parent, std::string signature) : JMetaObject(parent, signature){ }
 		SignalType emit(SignalType value = (void*)0) { return value; }
-
-	protected:
-		std::string _signature;
-		JObject* _parent;
 	};
 
 } /* namespace core */
